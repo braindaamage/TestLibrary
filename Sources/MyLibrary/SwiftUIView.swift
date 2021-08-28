e4f0cb37d8aea82b71b23eaf3f098af8dd07e2bb
Leonardo Olivares <lolivares@gobetterfly.com>
2021-08-27 23:34:30 -0400

    Initial commit

---
+++ Sources/MyLibrary/SwiftUIView.swift
@@ -0,0 +1,22 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Leonardo Olivares on 27-08-21.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    @available(iOS 13.0, *)
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    @available(iOS 13.0, *)
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
